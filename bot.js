4302a187bf2d345d75e11114ea78d0fb3564ca6a
1exuryai-art <1exuryai@gmail.com>
2026-04-10 14:19:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,23 +2,37 @@ import TelegramBot from 'node-telegram-bot-api';
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
+// ⚠️ ВСТАВЬ СЮДА СВОЙ RAILWAY ДОМЕН
+const MINI_APP_URL = 'instabots-production.up.railway.app';
+
 const bot = new TelegramBot(token, { polling: true });
 
 console.log('Bot started');
 
-bot.onText(/\/start/, (msg) => {
+bot.onText(/\/start/, async (msg) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, 'Бот работает. Нажми кнопку ниже.', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: 'Анкета',
-            web_app: { url: process.env.MINI_APP_URL }
-          }
-        ]
-      ]
-    }
-  });
+  try {
+    await bot.sendPhoto(
+      chatId,
+      'ankieta11.png', // ← файл в корне репо
+      {
+        caption: `Привет.
+
+Нажми кнопку ниже и открой анкету.`,
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: 'Открыть анкету',
+                web_app: { url: MINI_APP_URL }
+              }
+            ]
+          ]
+        }
+      }
+    );
+  } catch (e) {
+    console.error('ERROR:', e.message);
+  }
 });
