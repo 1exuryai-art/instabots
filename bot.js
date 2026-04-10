233255db724212cc2e4348b6f2acb6c950058f8a
1exuryai-art <1exuryai@gmail.com>
2026-04-10 13:46:07 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const MINI_APP_URL = process.env.MINI_APP_URL;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, async (msg) => {
+  const chatId = msg.chat.id;
+
+  await bot.sendPhoto(chatId, './welcome.jpg', {
+    caption: `Привет.
+
+Это анкета для разбора твоей системы обработки заявок в Instagram.
+
+Нажми кнопку ниже и открой анкету.`,
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Анкета',
+            web_app: { url: MINI_APP_URL }
+          }
+        ]
+      ]
+    }
+  });
+});
